b80326b358311e4ffbf6f8b503ae68eef336eaa7
cdmidemo <cdmi.demojs@gmail.com>
2023-09-01 09:05:28 +0530

    01/09/2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:math';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -12,7 +13,6 @@ void main() {
 
 class Wordpuzzle extends StatefulWidget {
   const Wordpuzzle({Key? key}) : super(key: key);
-
   @override
   State<Wordpuzzle> createState() => _WordpuzzleState();
 }
@@ -20,7 +20,13 @@ class Wordpuzzle extends StatefulWidget {
 class _WordpuzzleState extends State<Wordpuzzle> {
   String imagename = "";
 
+  Map map = {};
+
   bool vv = false;
+
+  List<String> anslist = [];
+  List<String> bottomlist = [];
+  List<String> toplist = [];
 
   @override
   void initState() {
@@ -65,13 +71,14 @@ class _WordpuzzleState extends State<Wordpuzzle> {
       // print("===${s2}");
       //
       String s2 = imagename.split("/")[1].split("\.")[0];
-      List<String> anslist = s2.split(""); // [d, o, g]
+      anslist = s2.split(""); // [d, o, g]
+      toplist = List.filled(anslist.length, "");
       print("==$anslist");
       String abcd = "abcdefghijklmnopqrstuvwxyz";
       List<String> absclist = abcd.split("");
       print("${absclist}");
       absclist.shuffle();
-      List bottomlist = absclist.getRange(0, 10 - anslist.length).toList();
+      bottomlist = absclist.getRange(0, 10 - anslist.length).toList();
       print("==${bottomlist}");
       bottomlist.addAll(anslist);
       bottomlist.shuffle();
@@ -83,8 +90,102 @@ class _WordpuzzleState extends State<Wordpuzzle> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: vv
-          ? Container(color: Colors.yellow, child: Image.asset(imagename))
+          ? Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(color: Colors.yellow, child: Image.asset(imagename)),
+                Container(
+                  height: 100,
+                  child: GridView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: anslist.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 1),
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {
+                          setState(() {
+                            if (toplist[index].isNotEmpty) {
+                              bottomlist[map[index]] = toplist[index];
+
+                              toplist[index] = "";
+                            }
+                          });
+                        },
+                        child: Container(
+                          margin: EdgeInsets.all(5),
+                          color: Colors.yellow,
+                          alignment: Alignment.center,
+                          child: Text(
+                            "${toplist[index]}".toUpperCase(),
+                            style: TextStyle(fontSize: 30),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Container(
+                  height: 200,
+                  child: GridView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: bottomlist.length,
+                    shrinkWrap: true,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2),
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {
+                          setState(() {
+                            if (bottomlist[index].isNotEmpty) {
+                              for (int a = 0; a < anslist.length; a++) {
+                                if (toplist[a] == "") {
+                                  toplist[a] = bottomlist[index];
+                                  bottomlist[index] = "";
+                                  // from = where
+                                  map[a] = index;
+                                  print("==${map}");
+                                  // {0:2,1:9,2:5}
+                                  break;
+                                }
+                              }
+
+                              if (listEquals(anslist, toplist)) {
+                                setState(() {
+                                  cc = Colors.green;
+                                });
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(content: Text("YOU Are Won")));
+                              } else if (!toplist.contains("")) {
+                                setState(() {
+                                  cc = Colors.red;
+                                });
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(content: Text("Try More")));
+                              }
+                            }
+                          });
+                        },
+                        child: Container(
+                          margin: EdgeInsets.all(5),
+                          color: cc,
+                          alignment: Alignment.center,
+                          child: Text(
+                            "${bottomlist[index]}".toUpperCase(),
+                            style: TextStyle(fontSize: 30),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                )
+              ],
+            )
           : Center(child: CircularProgressIndicator()),
     );
   }
+
+  Color cc = Colors.yellow;
 }
